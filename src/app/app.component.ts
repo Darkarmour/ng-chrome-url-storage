9275f55af2472ecac87d347e010b1a22b7df62a7
jp_darkarmour <jpchennai462@gmail.com>
2020-05-28 21:55:20 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
@@ -7,17 +7,62 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  urls: AngularFireList<string>;
-  url: string;
+  firebaseLinks: AngularFireList<string>;
+  links: Array<string> = [];
+  link: string;
+  isLoading: boolean = true;
+  alerts: {
+    success?: boolean,
+    error?: boolean
+  } = {};
+  alertMessage: {
+    success?: string,
+    error?: string
+  } = {};
 
-  constructor(db: AngularFireDatabase) {
-    this.urls = db.list('/urls')
-    console.log('Urls', this.urls);
+  constructor(
+    public db: AngularFireDatabase) {
+    this.firebaseLinks = db.list('/links');
   }
 
-  saveUrl() {
-    this.urls.push(this.url);
+  ngOnInit() {
+    this.getLinks();
+  }
+
+  getLinks() {
+    this.db.list('/links').snapshotChanges().subscribe(response => {
+      this.links = [];
+      if (response && response.length) {
+        for (let data of response) {
+          this.links.push((data.payload.val()).toString())
+        }
+      }
+      this.isLoading = false;
+      console.log(response)
+    })
+  }
+
+  saveLink() {
+    let isExistingLink: boolean = this.links.find(i => i === this.link) ? true : false;
+    if (isExistingLink) {
+      console.log(this.alerts)
+      this.showAlert('error', 'Link already exists.');
+    }
+    else {
+      this.firebaseLinks.push(this.link);
+      this.showAlert('success', `Saved Link!`);
+      this.link = '';
+    }
+  }
+
+  showAlert(type: string, message: string) {
+    this.alertMessage[type] = message;
+    this.alerts[type] = true;
+    setTimeout(() => {
+      this.alerts[type] = false;
+      this.alertMessage[type] = undefined;
+    }, 3000);
   }
 }
