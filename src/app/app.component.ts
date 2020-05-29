12a0fc95f3e5fbe9a19102c4c5b44d47284c5a1b
jp_darkarmour <jpchennai462@gmail.com>
2020-05-29 20:15:54 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
 
-  firebaseLinks: AngularFireList<string>;
-  links: Array<string> = [];
+  firebaseBookmarks: AngularFireList<Bookmark>;
+  bookmarks: Array<Bookmark> = [];
   link: string;
   isLoading: boolean = true;
   alerts: {
@@ -24,45 +25,54 @@ export class AppComponent implements OnInit {
 
   constructor(
     public db: AngularFireDatabase) {
-    this.firebaseLinks = db.list('/links');
+    this.firebaseBookmarks = db.list('/bookmarks');
   }
 
   ngOnInit() {
-    this.getLinks();
+    this.getBookmarks();
   }
 
-  getLinks() {
-    this.db.list('/links').snapshotChanges().subscribe(response => {
-      this.links = [];
+  getBookmarks() {
+    this.db.list('/bookmarks').snapshotChanges().subscribe(response => {
+      this.bookmarks = [];
       if (response && response.length) {
         for (let data of response) {
-          this.links.push((data.payload.val()).toString())
+          let firebaseData = data.payload.val();
+          let bookmark: Bookmark = {
+            link: firebaseData['link'],
+            updatedAt: firebaseData['updatedAt']
+          }
+          this.bookmarks.push(bookmark)
         }
       }
-      console.log('Links', this.links)
       this.isLoading = false;
     })
   }
 
-  saveLink() {
-    let isExistingLink: boolean = this.links.find(i => i === this.link) ? true : false;
+  saveBookmark() {
+    let isExistingBookmark: boolean = this.bookmarks.find(i => i.link === this.link) ? true : false;
     var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
     let urlRegex = new RegExp(expression);
-    if (isExistingLink) {
+    if (isExistingBookmark) {
       this.showAlert('error', 'Bookmark already exists.');
     }
     else if (!this.link.match(urlRegex)) {
       this.showAlert('error', 'Bookmark is not a valid link.');
     }
     else {
-      this.firebaseLinks.push(this.link);
+      let bookmark: Bookmark = {
+        link: this.link,
+        updatedAt: new Date().valueOf()
+
+      };
+      this.firebaseBookmarks.push(bookmark);
       this.showAlert('success', `Saved bookmark!`);
       this.link = '';
     }
   }
 
-  openLink(link: string) {
-    window.open(link);
+  openBookmark(bookmark: string) {
+    window.open(bookmark);
   }
 
   showAlert(type: string, message: string) {
@@ -73,4 +83,9 @@ export class AppComponent implements OnInit {
       this.alertMessage[type] = undefined;
     }, 3000);
   }
+}
+
+export interface Bookmark {
+  link: string
+  updatedAt: number
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
